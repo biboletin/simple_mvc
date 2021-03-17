8c8252a1a57a2a83150fcd8881d1739838b3f75e
bivanov <bhsfirm@gmail.com>
2021-03-17 09:54:14 +0200

    Implement sample config

---
+++ config.sample.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+    /**
+     * MySQL connection
+     */
+    'mysql' => [
+        /**
+         * Driver
+         *
+         * mysql
+         * mysqli
+         * pdo
+         */
+        'engine' => 'mysqli',
+        /**
+         * Host
+         */
+        'host' => '127.0.0.1',
+        /**
+         * User
+         */
+        'user' => 'root',
+        /**
+         * Password
+         */
+        'password' => '',
+        /**
+         * Database name
+         */
+        'database' => '',
+        /**
+         * Collation
+         */
+        'collation' => '',
+        /**
+         * Charset
+         */
+        'charset' => ''
+    ],
+    /**
+     * Session
+     */
+    'session' => [],
+    /**
+     * Security
+     */
+    'security' => [],
+];
