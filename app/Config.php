c6a50d691c9aaba5add76d94a2911a4ff1e9fa61
bivanov <bhsfirm@gmail.com>
2021-03-17 09:51:24 +0200

    Implement Config class. Create autoloader

--- app/Config.php
+++ app/Config.php
@@ -1,7 +1,30 @@
 <?php
 /**
- * Configuration class
+ *
+ */
+namespace App\Config;
+/**
+ * Class Config
+ * @package App
  */
 class Config
 {
+    /**
+     * @param string|null $search
+     * @return string
+     */
+    public static function get($search = null)
+    {
+        $config = include __DIR__ . '/config.php';
+        $parts = explode('.', $search);
+
+        foreach ($parts as $part) {
+            if (isset($config[$part])) {
+                $config = $config[$part];
+            } else {
+                $config = '';
+            }
+        }
+        return $config;
+    }
 }
